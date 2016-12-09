88adc220cb547bf7cde75875ae23b405dbbc7823
Yann Kaiser <yann.kaiser@epita.fr>
2016-12-09 03:40:49 +0100

    Updated the core.cc file in the kernel code

--- src/arch/x86_64/kernel/core.cc
+++ src/arch/x86_64/kernel/core.cc
@@ -5,8 +5,9 @@ extern "C"
 	void _start()
 	{
 		VGA vga;
-		vga.color_set(0x0F);
-		const char *text = "Hello, world !";
+		vga.color_set(0x3f);
+
+		const char *text = "Hello, world. It's kOS !";
 		vga.print(text);
 	}
 }
